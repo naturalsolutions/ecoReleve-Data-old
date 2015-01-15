8898259f8eecd0f78024f08356d1e07916de40a6
ktalbi <khaled_talbi@hotmail.com>
2015-01-15 16:17:24 +0100

    input : update new station form

--- modules/import/layouts/lyt-step5.js
+++ modules/import/layouts/lyt-step5.js
@@ -14,9 +14,18 @@ define([
         =            Layout Stepper Orchestrator            =
         ===================================================*/
 
+        events : {
+           // 'click button#btnNext' : 'navigate'
+        },
         onShow: function(){
             var msg = this.model.get('ajax_msg') ; 
             $('#importResponseMsg').text(msg); 
+        },
+        navigate : function(){
+            alert('clicked !');
+        },
+        nextOK: function(){
+            return true;
         }
     });
 
